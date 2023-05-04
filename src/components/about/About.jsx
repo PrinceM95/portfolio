5eb11d1c16bbbfad68c1fbb8cc36f135a0d8f660
PrinceM95 <moutimap@gmail.com>
2023-05-03 22:08:28 -0400

    portfolio

---
+++ src/components/about/About.jsx
@@ -0,0 +1,48 @@
+import React from 'react'
+import './about.css'
+import PHOTO from '../../assets/me.png'
+import {FaAward} from 'react-icons/fa'
+import {VscFolderLibrary} from 'react-icons/vsc'
+
+const About = () => {
+  return (
+    <section id='about'>
+      <h5>Get To Know</h5>
+      <h2>About Me</h2>
+
+      <div className="container about__container"> 
+        <div className="about__me">
+          <div className="about__me-image">
+            <img src={PHOTO} alt="About Image" />
+            </div>
+          </div>
+
+          <div className="about__content">
+            <div className="about__cards">
+              <article className='about__card'>
+                <FaAward className='about__icon'/>
+                <h5>Experience</h5>
+                <small>1+ Years Working</small>
+              </article>
+
+              <article className='about__card'>
+                <VscFolderLibrary className='about__icon'/>
+                <h5>Projects</h5>
+                <small>1+ Years Working</small>
+              </article>
+            </div>
+
+            <p>
+            An eager, dedicated learner transitioning into a Web Development role after completing We Can Code IT Bootcamp.  
+            Hands-on project experience building both the front-end and back-end of web apps using Node.js, React.js, 
+            ASP.NET, and other frameworks.
+            </p>
+
+            <a href="#contact" className='btn btn-primary'>Let's Talk</a>
+          </div>
+        </div>
+      </section>
+  )
+}
+
+export default About
