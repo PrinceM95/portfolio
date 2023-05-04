11129a31c516b5a9565183c34df19a60bb1ff7b7
PrinceM95 <moutimap@gmail.com>
2023-05-03 21:03:22 -0400

    the commit continues

---
+++ personal-portfolio/src/components/about/About.jsx
@@ -0,0 +1,48 @@
+import React from 'react'
+import './about.css'
+import PHOTO from '../../assets/me.png'
+import {FaAward} from 'react-icons/fa'
+import {VscFolderLibrary} from 'react-icons/vsc'
+
+const About = () => {
+  return (
+    <section id='about'>
+      <h5>Get To Know</h5>
+      <h2>About Me</h2>
+
+      <div className="container about__container"> 
+        <div className="about__me">
+          <div className="about__me-image">
+            <img src={PHOTO} alt="About Image" />
+            </div>
+          </div>
+
+          <div className="about__content">
+            <div className="about__cards">
+              <article className='about__card'>
+                <FaAward className='about__icon'/>
+                <h5>Experience</h5>
+                <small>3+ Years Working</small>
+              </article>
+
+              <article className='about__card'>
+                <VscFolderLibrary className='about__icon'/>
+                <h5>Projects</h5>
+                <small>3+ Years Working</small>
+              </article>
+            </div>
+
+            <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut 
+            labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco 
+            laboris nisi ut aliquip ex ea commodo consequat. 
+            </p>
+
+            <a href="#contact" className='btn btn-primary'>Let's Talk</a>
+          </div>
+        </div>
+      </section>
+  )
+}
+
+export default About
---
+++ personal-portfolio/src/components/header/CTA.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import CV from '../../assets/resume.pdf'
+
+const CTA = () => {
+  return (
+    <div className='cta'>
+        <a href={CV} download className='btn'> Resume</a>
+        <a href="#contact" className='btn btn-primary'> Let's talk</a>
+    </div>
+  )
+}
+
+export default CTA
