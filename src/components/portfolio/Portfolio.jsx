909c85d510799956ab96fb5fa21e8e407ac99c53
PrinceM95 <moutimap@gmail.com>
2023-06-19 14:02:00 -0400

    added descriptions

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -9,19 +9,22 @@ const data = [
     id: 1,
     image:  IMG1,
     title: 'Geo-To-Go',
-    github:'https://github.com/PrinceM95/Geo-To-Go'
+    github:'https://github.com/PrinceM95/Geo-To-Go',
+    bio: 'Geo-To-Go is an educational platform dedicated to enlightening individuals about countries, continents, states, capitals, and general knowledge associated with geography. With a primary focus on elementary to middle school learners, this website offers valuable resources for anyone seeking to enhance their understanding of capitals and flags. Whether you are a student, educator, or simply curious about geography, Geo-To-Go provides an accessible and enriching experience for all.'
   },
   {
     id: 2,
     image:  IMG2,
     title: 'Jacs Mystery Educator',
-    github:'https://github.com/PrinceM95/Mystery-Educator'
+    github:'https://github.com/PrinceM95/Mystery-Educator',
+    bio: 'Mystery Educator is a specialized platform created with the sole purpose of educating children between the ages of 6 and 13. Our goal is to provide engaging and informative content on a wide range of topics, from astronomy to exploring various musical instruments, utilizing the power of APIs. Designed to captivate young minds, Mystery Educator offers a unique learning experience that combines fun, curiosity, and knowledge acquisition. Join us on this educational journey and unlock the wonders of the world through interactive exploration.'
   },
   {
     id: 3,
     image:  IMG3,
     title: 'Island Hopping Adventures',
-    github:'https://github.com/PrinceM95/first-team-project-team-island-hnpw'
+    github:'https://github.com/PrinceM95/first-team-project-team-island-hnpw',
+    bio: 'A site that’s been created for individuals who seek to gain general knowledge on various islands around the globe. Users possess the option to search islands based off their respective continents and countries.'
   }
 ]
 
@@ -33,13 +36,14 @@ const portfolio = () => {
 
       <div className="container portfolio__container">
         {
-          data.map(({id, image, title, github}) => {
+          data.map(({id, image, title, github, bio}) => {
             return (
               <article key={id} className='portfolio__item'>
               <div className="portfolio__item-image">
                 <img src={image} alt={title} />
               </div>
           <h3>{title}</h3>
+          <p1>{bio}</p1>
           <div className="portfolio__item-cta">
             <a href={github} className='btn' target='_blank'>Github</a>
           </div>
